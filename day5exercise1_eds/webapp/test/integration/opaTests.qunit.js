3ac442b90211179264d1c7dbdb4782435d4a1bd2
Edrian Apilan <edrian.apilan@accenture.com>
2023-03-20 03:21:22 +0000

    Initial Commit

--- day5exercise1_eds/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["zbtp/day5exercise1eds/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- day5exercise1_eds/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"zbtp/day5exercise1_eds/test/unit/controller/Home.controller"
-], function () {
-	"use strict";
-});
